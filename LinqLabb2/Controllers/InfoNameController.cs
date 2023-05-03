5aa2f6d6999916d9fb1d5e68cecbacb6350f1b35
malin eriksson <erixon.malin@hotmail.com>
2023-05-03 11:40:39 +0200

    MoreFixes

--- LinqLabb2/Controllers/InfoNameController.cs
+++ LinqLabb2/Controllers/InfoNameController.cs
@@ -80,9 +80,57 @@ namespace LinqLabb2.Controllers
 
             return View(list);
         }
-        //public async Task<IActionResult> SearchStudent()
-        //{
-        //    return View();
-        //}
+
+        public IActionResult UpdateTeacher()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ChangeTeacher(string CourseName, string TLastName, string ClassName)
+        {
+
+            var currentTeacher = (from t in schoolDbContext.Teachers
+                                 join cl in schoolDbContext.Classes on t.TeacherId equals cl.FK_TeacherId
+                                 join c in schoolDbContext.Courses on cl.FK_CourseId equals c.CourseId
+                                 join s in schoolDbContext.Students on cl.ClassId equals s.FK_ClassId
+                                 where t.TeacherId == cl.FK_TeacherId && c.CourseName == CourseName && cl.ClassId == s.FK_ClassId && cl.ClassName == ClassName
+                                 select t).FirstOrDefault();
+
+            var teacherNew = (from t in schoolDbContext.Teachers
+                              where t.TLastName == TLastName
+                              select t).FirstOrDefault();
+
+            var teacherUpdate = (from cl in schoolDbContext.Classes
+                                 where cl.FK_TeacherId  == currentTeacher.TeacherId
+                                 select cl).FirstOrDefault();
+
+            teacherUpdate.FK_TeacherId = teacherNew.TeacherId;
+            await schoolDbContext.SaveChangesAsync();
+
+            return View();
+
+        }
+
+        public IActionResult UpdateCourseName()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ChangeCourseName(string CurrentCourseName, string NewCourseName)
+        {
+            var course = (from c in schoolDbContext.Courses
+                          where c.CourseName == CurrentCourseName
+                          select c).FirstOrDefault();
+
+            if (course == null)
+            {
+                return NotFound("Tyvärr hittades inte kursen, vänligen försök igen");
+            }
+
+            course.CourseName = NewCourseName;
+            await schoolDbContext.SaveChangesAsync();
+
+            return View();
+        }
     }
 }
